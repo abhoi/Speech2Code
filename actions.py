31aa3eec87eab38bc908d9d8ef7f26549a7ad6fb
Debojit Kaushik <kaushik.debojit@gmail.com>
2018-10-20 10:10:34 -0400

    Actions added for 7 cases

--- actions.py
+++ actions.py
@@ -10,17 +10,76 @@ class Action:
     def get_action(intent_data):
         try:
             action_dic = None
-            print("Here", intent_data)
-            if intent_data["topScoringIntent"]["intent"] == "add_function":
-                action_dic = {
-                    "status": "Function Creation Returned",
-                    "action": "add_fun",
-                    "data": {
-                        "func_name": [i["entity"] for i in intent_data["entities"] if i["type"] == "function_name"][0],
-                        "args": [i["entity"] for i in intent_data["entities"] if i["type"] == "argument"]
+            intents = [
+                "add_function",
+                "add_if_else",
+                "add_main",
+                "add_while",
+                "goto",
+                "call_function",
+                "add_variables" 
+            ]
+            intent = intent_data["topScoringIntent"]["intent"]
+            if intent in intents:
+                if intent == "add_function":
+                    action_dic = {
+                        "status": "Function Creation Returned",
+                        "action": "add_fun",
+                        "data": {
+                            "func_name": [i["entity"] for i in intent_data["entities"] if i["type"] == "function_name"][0],
+                            "args": [i["entity"] for i in intent_data["entities"] if i["type"] == "argument"]
+                        }
                     }
-                }
-            print(action_dic)
+                elif intent == "add_if_else":
+                    action_dic = {
+                        "status": "Function Creation Returned",
+                        "action": "add_if_else",
+                        "data": {}
+                    }
+                elif intent == "add_main":
+                    action_dic = {
+                        "status": "Function Creation Returned",
+                        "action": "add_main",
+                        "data": {}
+                    }
+                elif intent == "add_while":
+                    action_dic = {
+                        "status": "While statement added.",
+                        "action": "add_while",
+                        "data": {
+                            "args": intent_data["query"]                            
+                        }
+                    }
+                elif intent == "goto":
+                    action_dic = {
+                        "status": "Go to line/function called.",
+                        "action": "goto",
+                        "data": {
+                            "args": intent_data["entities"][0]["entity"],
+                            "type": intent_data["entities"][0]["type"]                         
+                        }
+                    }
+                elif intent == "call_function":
+                    action_dic = {
+                        "status": "Function called.",
+                        "action": "call_function",
+                        "data": {
+                            "args": [{"entity": item["entity"], "type": item["type"]} for item in intent_data["entities"]]                            
+                        }
+                    }
+                elif intent == "add_variables":
+                    action_dic = {
+                        "status": "Variable added.",
+                        "action": "add_variable",
+                        "data": {
+                            "args": {
+                                "entity": intent_data["entities"]["entity"],
+                                "type": intent_data["entities"]["type"]
+                            }
+                        }
+                    }
+                else:
+                    print(intent)
             return action_dic
         except Exception:
             print(traceback.format_exc())
--- message_broker.py
+++ message_broker.py
@@ -32,7 +32,7 @@ class MessageBroker:
             if action_dic["action"] == "init":
                 MessageBroker.send_message("py_to_ele", json_data = json.dumps({'status': 'listening'}))
                 #INSERT AMLAANS FUNCTION CALL.
-                res = Text2CodeRequest._create_to_code_request("Create a function X with argument alpha, beta and gamma that returns Y", HEADERS, PARAMS)
+                res = Text2CodeRequest._create_to_code_request("create a main function", HEADERS, PARAMS)
                 action_data = Action.get_action(res)
                 print(action_data)
                 MessageBroker.send_message("py_to_ele", json.dumps(action_data))
